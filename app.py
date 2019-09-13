99bee27d1702babb0756b56655cd41a17253cb6b
Ubuntu <ubuntu@ip-172-31-21-228.ec2.internal>
2019-09-13 08:45:46 +0000

    Added addtask.html

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
-@app.route('/')
-def hello():
-    return 'Hello World ...again'
+# Create an instance of PyMongo. Add the app into that with a constructor method.
+mongo = PyMongo(app)
+
+"""
+- Make connection to the database
+- render a template (tasks.html)
+- supply a tasks collection (tasks=mongo.db.tasks,), which will be returned from making a call directly to Mongo.
+- with the find() method, which will return everything.
+"""
+@app.route('/') # this is called a decorator
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
